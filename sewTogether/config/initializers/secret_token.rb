64dbe83b90de691cb8757f3980bb73861d50f41b
Kaleb Ayalew <kayalew@mit.edu>
2013-04-28 05:14:26 -0400

    added new rails app

---
+++ sewTogether/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SewTogether::Application.config.secret_token = '0725d4e2fb2ef6c4a428f2000b7b6ce05be8620883f2cef0e7d8ee2fa16f86da14a894d63e596cfcf724f8113d3430b53a239f160debe0ab7d9db679c2b8d23e'
