95c47deb784bbdd4f90148b744bfc0198261341a
Kaleb Ayalew <kayalew@mit.edu>
2013-04-29 13:32:06 -0400

    moved projects to its own controller

---
+++ sewTogether/app/controllers/projects_controller.rb
@@ -0,0 +1,17 @@
+class ProjectsController < ApplicationController
+  #for now building everything as a static page
+  def overview
+  end
+  def step0
+  end
+  def step1
+  end
+  def step2
+  end
+  def step3
+  end
+  def step4
+  end
+  def step5
+  end
+end
--- sewTogether/app/controllers/static_pages_controller.rb
+++ sewTogether/app/controllers/static_pages_controller.rb
@@ -9,19 +9,4 @@ class StaticPagesController < ApplicationController
   end
   def about
   end
-  #for now building everything as a static page
-  def overview
-  end
-  def step0
-  end
-  def step1
-  end
-  def step2
-  end
-  def step3
-  end
-  def step4
-  end
-  def step5
-  end
 end
