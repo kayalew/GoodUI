5c5a4419a9fbb03dff02bc40a41e26ec762482a2
Xingyi Li <xingyi@mit.edu>
2013-05-01 17:12:02 -0400

    fixed merge conflict

--- sewTogether/app/controllers/application_controller.rb
+++ sewTogether/app/controllers/application_controller.rb
@@ -2,16 +2,8 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery
 	
 	private
-<<<<<<< HEAD
 		def current_user
 			@current_user ||= session[:user_id] && User.find_by_id(session[:user_id])
 		end
 		helper_method :current_user
-	
-=======
-	def current_user
-		@current_user ||= session[:user_id] && User.find_by_id(session[:user_id])
-	end
-	helper_method :current_user
->>>>>>> 553c98555a4732ea21b6169a0eb1fbdb9ac073a5
 end
