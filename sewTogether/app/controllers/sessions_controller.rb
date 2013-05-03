a43fb6c225638ced6a3a95f8578ddfab4c185426
Tristan Daniels <tristan7@mit.edu>
2013-05-03 05:22:25 -0400

    Can create new user, roots to dashboard

--- sewTogether/app/controllers/sessions_controller.rb
+++ sewTogether/app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
 	def new
 	end
+	
 	def create
 		user = User.find_by_email(params[:email])
-		if user && user.authenticate(params[:user_name], params[:password])
-			session[:user_id] = user.id
+		if user && User.authenticate(params[:user_name], params[:password])
+			session[:current_user_id] = user.id
 			redirect_to root_url
 		else
 			flash.now.alert = 'Invalid email or password'
@@ -12,7 +13,7 @@ class SessionsController < ApplicationController
 		end
 	end
 	def destroy
-		session[:user_id] = nil
+		@_current_user = session[:current_user_id] = nil
 		redirect_to root_url
 	end
 end
--- sewTogether/db/schema.rb
+++ sewTogether/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130501181629) do
+ActiveRecord::Schema.define(:version => 20130503090532) do
 
   create_table "glossary_entries", :force => true do |t|
     t.string   "name"
@@ -38,18 +38,21 @@ ActiveRecord::Schema.define(:version => 20130501181629) do
   end
 
   create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.string   "name"
-    t.string   "password"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
-    t.string   "password_confirmation"
+    t.string   "password_digest"
     t.string   "user_name"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
