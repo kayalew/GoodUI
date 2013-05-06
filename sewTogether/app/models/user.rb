ce2d9b132c3da71230579ffbdf0794eddd22fa6c
Tristan Daniels <tristan7@mit.edu>
2013-05-06 15:58:41 -0400

    Users must now be created with email and username

--- sewTogether/app/models/user.rb
+++ sewTogether/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :user_name
   has_secure_password
+  validates_presence_of :email, :on => :create
   validates_presence_of :password, :on => :create
-  has_many :comments 
+  validates_presence_of :user_name, :on => :create
 end
