832bbae0714d45e8b75e7bb0929d8e85a385bd5a
Tristan Daniels <tristan7@mit.edu>
2013-05-06 16:03:13 -0400

    Updated application_controller

--- sewTogether/app/controllers/application_controller.rb
+++ sewTogether/app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery
 	
 	private
+	
 	def current_user
-		@current_user ||= session[:current_user_id] && User.find_by_id(session[:current_user_id])
+		@current_user ||= User.find_by_id(session[:session_id]) if session[:session_id]
 	end
 	helper_method :current_user
 end
