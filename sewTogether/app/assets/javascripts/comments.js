32344afaffeae76943bc2cf40a18dc17584d9b99
Kaleb Ayalew <kayalew@mit.edu>
2013-05-06 14:32:24 -0400

    changed "comments" header to "notes", changed newComment class to commentSubmit, enabled comment box to slide down when clicked

---
+++ sewTogether/app/assets/javascripts/comments.js
@@ -0,0 +1,14 @@
+//http://stackoverflow.com/questions/7151491/restrict-the-slidetoggle-animation-at-once-when-click-the-radiobutton
+//http://api.jquery.com/slideDown/
+function setupCommentBox() {
+	$("#commentBox").hide();
+	setupCommentBoxToggle();
+}
+function setupCommentBoxToggle() {
+  jQuery("#commentSubmit").click(function()
+  {
+	$("#commentBox").slideDown(400);
+	return false;
+  });
+}
+
