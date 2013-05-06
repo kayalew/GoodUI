32f60f093c2873707d4f42b76b2aed1b76ed93f1
Tristan Daniels <tristan7@mit.edu>
2013-05-06 15:56:41 -0400

    Updated sessions_controller

--- sewTogether/app/controllers/sessions_controller.rb
+++ sewTogether/app/controllers/sessions_controller.rb
@@ -3,16 +3,17 @@ class SessionsController < ApplicationController
 	end
 	
 	def create
-		user = User.find_by_email(params[:email])
-		if user && User.authenticate(params[:user_name], params[:password])
-			session[:current_user_id] = user.id
-			redirect_to root_url
+		user = User.find_by_user_name(params[:user_name])
+		if user && user.authenticate(params[:password])
+			session[:session_id] = user.id
+			@current_user = current_user 
+			redirect_to dashboard_static_pages_path, :notice => 'Welcome back, '+@current_user.user_name+'!'
 		else
-			flash.now.alert = 'Invalid email or password'
+			redirect_to root_url, :notice => 'Could not log in: error message'
 		end
 	end
 	def destroy
-		@_current_user = session[:current_user_id] = nil
-		redirect_to root_url
+		@current_user = session[:session_id] = nil
+		redirect_to root_url, :notice => 'Logged out!'
 	end
 end
