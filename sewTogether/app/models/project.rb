3f21d3f0fc2532e6a4e9f134fb2894d333d8e3fa
Kaleb Ayalew <kayalew@mit.edu>
2013-05-07 18:29:14 -0400

    added todo if we develop project further

--- sewTogether/app/models/project.rb
+++ sewTogether/app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :num_steps
   has_many :comments
   has_many :users, :through => :project_selections
+  #TODO: if steps moves to db count
+  #them instead of having num_steps
 end
