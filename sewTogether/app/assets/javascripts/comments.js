8050b2486fd6b27305c51c0985e0664fbb65e5bb
Kaleb Ayalew <kayalew@mit.edu>
2013-05-08 04:58:04 -0400

    comments js was returning false (and i believe overriding the default button behavior). i modifed code to return true if commentBox is visible and (renamed and) moved it to comments partial so that it would refresh upon each partial re-rendering (and make the comment partial less reliant on other pieces of code, to hide it etc). also, deleted the select_project links from show

--- sewTogether/app/assets/javascripts/comments.js
+++ sewTogether/app/assets/javascripts/comments.js
@@ -1,16 +1,21 @@
 //http://stackoverflow.com/questions/7151491/restrict-the-slidetoggle-animation-at-once-when-click-the-radiobutton
 //http://api.jquery.com/slideDown/
-function setupCommentBox() {
-	$("#commentBox").hide();
-	setupCommentBoxToggle();
-}
-function setupCommentBoxToggle() {
+function resetCommentBox() {
+  $("#commentBox").hide();
   jQuery("#commentSubmit").click(function()
   {
-	$("#commentBox").slideDown(400,function(){
-		$("#commentSubmit").val("Post new note");
-	});
-	return false;
+	//note: putting comment box into a span does weird things, 
+	//in chrome will make it default hidden (and not in firefox). 
+	//which is fine for this code, but will be good
+	//to remeber if this js evolves
+	if ($("#commentBox").is(':hidden') || $("#commentBox").val()==""){
+		$("#commentBox").slideDown(400,function(){
+			$("#commentSubmit").val("Post new note");
+		});
+		return false;
+	}
+	else{
+		return true;
+	}
   });
 }
-
