b0be42ac5e102ef8089002f261a56083d8dd9e23
Kaleb Ayalew <kayalew@mit.edu>
2013-05-14 22:24:31 -0400

    modified the formatting of steps.js (because of the code i added)

--- app/assets/javascripts/steps.js
+++ app/assets/javascripts/steps.js
@@ -3,32 +3,32 @@ $(document).ready(function() {
 
 	$(document).keydown(function(e){
 		if ($("#commentBox").is(':hidden') || $("#commentBox").val()=="" || !$("#commentBox").is(':focus')){
-		// navigation between steps w/  arrow keys	
-		// left arrow
-		if (e.which==37){
-			// step back if you can.
-			if ($("#stepBack").length!=0)
-				$("#stepBack").click();
-		}
-		// right arrow
-		else if (e.which==39){
-			// step next if you can.
-			if ($("#stepNext").length!=0)
-				$("#stepNext").click();
-			// or if you have the begin button instead
-			else if ($("#begin").length!=0)
-				$("#begin").click();
-		}
-		
-		/*
-		// pressing "/" on the keyboard opens up help and
-		// goes through them, maybe?
-		else if (e.which==191){
-			$(".glossaryTerm").each(function(i){
-				$(this).addClass("glossaryTermFocus");
-			});
-		}
-		*/
+			// navigation between steps w/  arrow keys	
+			// left arrow
+			if (e.which==37){
+				// step back if you can.
+				if ($("#stepBack").length!=0)
+					$("#stepBack").click();
+			}
+			// right arrow
+			else if (e.which==39){
+				// step next if you can.
+				if ($("#stepNext").length!=0)
+					$("#stepNext").click();
+				// or if you have the begin button instead
+				else if ($("#begin").length!=0)
+					$("#begin").click();
+			}
+			
+			/*
+			// pressing "/" on the keyboard opens up help and
+			// goes through them, maybe?
+			else if (e.which==191){
+				$(".glossaryTerm").each(function(i){
+					$(this).addClass("glossaryTermFocus");
+				});
+			}
+			*/
 		}
 		
 	});	
